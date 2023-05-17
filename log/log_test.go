126c48392638bf9183fe98d568d93ca6642164e5
Jason Li <changsongliqd@gmail.com>
2023-05-18 06:36:07 +0800

    test db

--- log/log_test.go
+++ log/log_test.go
@@ -1,6 +1,9 @@
 package log
 
 import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 	"os"
 	"testing"
 )
@@ -12,4 +15,13 @@ func TestLogger(t *testing.T) {
 	l.Errorf("this a error msg")
 
 	l.Infof(os.Getenv("USERNAME"))
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(127.0.0.1:3306)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("USERNAME"),
+		os.Getenv("PASSWORD"),
+		os.Getenv("DATABASE"),
+	)
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	fmt.Println("err")
+	fmt.Println(err)
 }
